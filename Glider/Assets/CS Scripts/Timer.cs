164c5508b59ca0bb7491b54553bb62d51f070464
Hagop100 <htanashian@gmail.com>
2022-10-26 20:05:05 -0700

    UI
    Timer
    Canvas

---
+++ Glider/Assets/CS Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    [SerializeField] float timerStart = 0f;
+    [SerializeField] Text timerText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimerTextHandler();
+    }
+
+    //Handles the timer text in the UI Canvas
+    //Find the time counting down
+    //convert to string
+    //set the the text in the UI
+    private void TimerTextHandler()
+    {
+        int timeCountDown = Mathf.FloorToInt(timerStart - Time.time);
+        string s = string.Format("Timer: {0} s", timeCountDown);
+        timerText.text = s;
+    }
+}
