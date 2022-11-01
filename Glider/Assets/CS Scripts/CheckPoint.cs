ac610d546880818a68827e712cbe616fa4a59b83
Hagop100 <htanashian@gmail.com>
2022-10-31 20:17:02 -0700

    tidying up

--- Glider/Assets/CS Scripts/CheckPoint.cs
+++ Glider/Assets/CS Scripts/CheckPoint.cs
@@ -7,20 +7,24 @@ public class CheckPoint : MonoBehaviour
 
     private DeathZone deathZone;
     private Vector2 checkPointPosition;
+    private SpriteRenderer mySpriteRenderer;
 
     private void Awake()
     {
         deathZone = FindObjectOfType<DeathZone>();
+        mySpriteRenderer = this.GetComponent<SpriteRenderer>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        mySpriteRenderer.color = Color.red;
         checkPointPosition = this.transform.position;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        mySpriteRenderer.color = Color.white;
         deathZone.ResetPlayerPosition(checkPointPosition);
     }
 }
